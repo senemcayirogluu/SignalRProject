e4768bb1b52f79f1b4ff95491ce46ab4c6020bc3
senemcayirogluu <senemcayiroglu@gmail.com>
2025-07-28 13:36:33 +0300

    Default About, BookATable, Offer, OurMenu, Slider, Testimonial Components added

--- SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
+++ SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9871a1f492ac6b8aebf85df8f3b2d0930b3d97c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+920bb31da22fec7de880822ddd567fcd76d098e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
