58358d59ac625588945ab5b1d56ce8c4d4d21dd4
senemcayirogluu <senemcayiroglu@gmail.com>
2025-07-31 09:15:20 +0300

    BookATable added to SignalRWebUI

--- SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
+++ SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78b0d5dbd925d5d18f4f1566cb9ebdad19d8f5a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+300036d256343b714301fc2d134d78a55aa6c77f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
