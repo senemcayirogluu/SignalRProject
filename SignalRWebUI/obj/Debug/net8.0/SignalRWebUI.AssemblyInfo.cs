7484aacec9e0c0266f6299829657f9f78e8b77b7
senemcayirogluu <senemcayiroglu@gmail.com>
2025-08-05 18:58:58 +0300

    MenuTable with SignalR added

--- SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
+++ SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e678be8de143dc5eb97962dac8fdea76799a2965")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e837d623541448b249e96faa75b743247fc8beb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
