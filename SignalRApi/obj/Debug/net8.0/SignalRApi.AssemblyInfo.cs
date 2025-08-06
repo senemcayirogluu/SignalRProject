971a45286ac2569a72144f2729987ba31a10f156
senemcayirogluu <senemcayiroglu@gmail.com>
2025-08-06 18:36:39 +0300

    ClientCount with SignalR added

--- SignalRApi/obj/Debug/net8.0/SignalRApi.AssemblyInfo.cs
+++ SignalRApi/obj/Debug/net8.0/SignalRApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7484aacec9e0c0266f6299829657f9f78e8b77b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a24eaf5f53ef96b8750bb8054a6fc5fdbf1ce7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
+++ SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a7770ba016f7cbb481eda891f46a5a772ecf971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a24eaf5f53ef96b8750bb8054a6fc5fdbf1ce7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
