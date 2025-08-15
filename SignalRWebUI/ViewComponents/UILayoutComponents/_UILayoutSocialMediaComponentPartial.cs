e87212beecd38c852cf0e27f095a624d229861aa
senemcayirogluu <senemcayiroglu@gmail.com>
2025-08-15 11:40:44 +0300

    Social Media links listed

---
+++ SignalRWebUI/ViewComponents/UILayoutComponents/_UILayoutSocialMediaComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.SocialMediaDto;
+
+namespace SignalRWebUI.ViewComponents.UILayoutComponents
+{
+	public class _UILayoutSocialMediaComponentPartial: ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		public _UILayoutSocialMediaComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7122/api/SocialMedia");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+
+				var values = JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData);
+				return View(values);
+			}
+			return View(new List<ResultSocialMediaDto>());
+		}
+	}
+}
--- SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
+++ SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26ffeba88e747312eaee456df2d2c4e733bafb95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5afd41a618b94d8bbbf22978fd9515530c2018a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
