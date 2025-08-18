e7327c04a538e63f1a4a7d66794e736465c14a8e
senemcayirogluu <senemcayiroglu@gmail.com>
2025-08-18 13:18:16 +0300

    QR Code controller added

--- SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
+++ SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de37b0bd7bfdd61752f2cf04cbb1576d17ef3297")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87664fa3f5fcb831239f0d5cc51ae4b933dbe664")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
